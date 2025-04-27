d04e0c3d27c5bf1a07c95c495b16dbacadc3cad0
LorayneNeves <lorayne3.8neves@outlook.com>
2025-04-27 16:42:16 -0300

    Remover

--- ControleDeVendas/Domain/Entities/ItemVenda.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ControleDeVendas.Domain.Entities
-{
-    public class ItemVenda
-    {
-        public Produto Produto { get; set; }
-        public int Quantidade { get; set; }
-        public decimal Total => Produto.Preco * Quantidade;
-    }
-}
